083b9beba80d49933c775765588f05dae9116fe7
b7urquieta <b7urquieta@gmail.com>
2022-08-18 10:02:46 -0500

    able to upload new java files

---
+++ java/java-01/repo/Prueba1/src/main/java/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        //System.out.println("Hola mundo Java");
+        String nombre = "Brandon";
+        System.out.println("saludos desde Java a: " + nombre);
+    }
+}
---
+++ java/java-01/repo/ejemploJdk/Hola.java
@@ -0,0 +1,13 @@
+public class Hola{
+
+// esto es una clase del JDK de Java
+//String miVariable = "Hola mundo java ";
+	public static void main (String [] arg){
+	// String miVariable = "Hola mundo java saludo a: ";
+	String miNombre = "Brandon";
+	// es la declaracion del metodo main
+		System.out.println ("Hola saludos a: " + miNombre);
+
+	}
+
+}
